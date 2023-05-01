edea2376b24be3f2a401ed5065f79951fa48a3a4
Ian Greenburg <93556547+iangreenslalom@users.noreply.github.com>
2023-05-01 10:58:56 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Cat Sounds in Different Cultures')
-streamlit.text('Meow')
-streamlit.text('Nya')
-streamlit.text('Wan')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
