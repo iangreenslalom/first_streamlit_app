952c2b59980558da7c482631fa512e035db2122a
Ian Greenburg <93556547+iangreenslalom@users.noreply.github.com>
2023-05-01 10:57:08 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Cat Sounds in Different Cultures')
+streamlit.text('Meow')
+streamlit.text('Nya')
+streamlit.text('Wan')
